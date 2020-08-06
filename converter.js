162fe1032aec924a9d7c0170db8c9de9015b7bf9
Romitou <romitou@protonmail.com>
2020-08-06 23:07:14 +0200

    :sparkles: More improvements

--- converter.js
+++ converter.js
@@ -5,22 +5,33 @@
 
 async function parse() {
     const code = document.getElementById('input').value.split('\n');
-    const isTab = document.getElementById('tabulations').checked;
+    const softIndent = document.getElementById('tabulations').checked ? '\t' : '    ';
+
+    let player;
     let output = [];
     let indentation = '';
     let guiSection = false;
+
+    code.push(''); // Ajout d'une ligne vide
     for (let line of code) {
-        if (/open virtual/gim.test(line)) {
+        if (!(line.startsWith(' ') || line.startsWith('\t')) && guiSection) {
+            output.push(indentation + 'open last created gui to ' + player[0]);
+            guiSection = false;
+        } else if (/open virtual/gim.test(line)) {
+            player = /(player|executor|victim|attacker|{.*?})/gim.exec(line);
             line = line.replace(/ (of|to) (player|executor|victim|attacker|{.*?})/gim, '');
             line = line.replace(/open virtual/gim, 'create a gui with virtual') + ':';
             indentation = line.match(/\s+/g)[0];
             guiSection = true;
         } else if (/(make|format|create)( a)? gui slot/gim.test(line)) {
             line = line.replace(/ (of|to) (player|executor|victim|attacker|{.*?})/gim, '');
+            line = line.replace(/to (do)? nothing/gim, '');
             line = line.replace(/(make|format|create)( a)? gui slot/gim, 'make gui slot');
-            line = (isTab ? '\t' : '    ') + line;
+            line = line.replace(/ to close( then)?/gim, ':\n' + indentation + softIndent.repeat(2) + 'close ' + player[0] + '\'s inventory');
+            line = line.replace(/(run|exec|execute) (function)?/gim, ':\n' + indentation + softIndent.repeat(2));
+            line = softIndent + line;
         } else if (guiSection) {
-            line = (isTab ? '\t' : '    ') + line;
+            line = softIndent + line;
         }
         output.push(line);
     }

5a1e1e5792eaa20b81367950d56d74e744837579
Romitou <romitou@protonmail.com>
2020-08-17 21:01:25 +0200

    :bug: Fixing a bug with optional pattern (fixes #1)

--- converter.js
+++ converter.js
@@ -1,8 +1,9 @@
 /**
- * Created by Romitou on 3/8/20.
- * Work in progress.
+ * @name parse
+ * @description Analyze the code and transform it.
+ * @async
+ * @author Romitou
  */
-
 async function parse() {
     const code = document.getElementById('input').value.split('\n');
     const softIndent = document.getElementById('tabulations').checked ? '\t' : '    ';
@@ -12,15 +13,20 @@ async function parse() {
     let indentation = '';
     let guiSection = false;
 
-    code.push(''); // Ajout d'une ligne vide
+    code.push(''); // Adding a new blank line to avoid errors with GUI section.
     for (let line of code) {
         if (!(line.startsWith(' ') || line.startsWith('\t')) && guiSection) {
             output.push(indentation + 'open last created gui to ' + player[0]);
             guiSection = false;
         } else if (/open virtual/gim.test(line)) {
             player = /(player|executor|victim|attacker|{.*?})/gim.exec(line);
+            let virtualInventory = /.*?(?<= with| named| to)/gim.exec(line)[0]; // We retrieve only the interesting part.
+            virtualInventory = virtualInventory.split(' '); // We split this into an array.
+            if (virtualInventory[virtualInventory.length - 2] !== 'inventory') { // We check if the effect contains the inventory pattern. Otherwise, we will add it below.
+                virtualInventory.splice(virtualInventory.length - 1, 0, 'inventory'); // We put the 'inventory' keyword before the last match of the array.
+                line = line.replace(/.*?(?<=with|named|to)/gim.exec(line)[0] || '', virtualInventory.join(' ')); // We join the all and replace it!
+            }
             line = line.replace(/ (of|to) (player|executor|victim|attacker|{.*?})/gim, '');
-            line = line.replace(/virtual .+/gim, 'virtual ' + line.match(/virtual .+/gim)[0].split(' ')[1] + ' inventory');
             line = line.replace(/open virtual/gim, 'create a gui with virtual') + ':';
             indentation = line.match(/\s+/g)[0];
             guiSection = true;
@@ -40,6 +46,13 @@ async function parse() {
     return output.join('\n');
 }
 
+/**
+ * @name convert
+ * @description Converted the requested code using the parser function.
+ * @see parse
+ * @async
+ * @author Romitou
+ */
 async function convert() {
     const error = document.getElementById('error');
     const success = document.getElementById('success');
